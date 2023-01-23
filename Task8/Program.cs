a7f0549c80a4b6fc565760338ac158f1f6fec2ee
AstonDV <astondv.kms@gmail.com>
2023-01-23 21:10:17 +1000

    End task 8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 2;
+Console.Clear();
+while(count <= number)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
