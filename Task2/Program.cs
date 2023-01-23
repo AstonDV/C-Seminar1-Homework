46b5945b9081e961e4f3eebe88f18aa2c6681019
AstonDV <astondv.kms@gmail.com>
2023-01-23 20:20:21 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+if (firstNumber > secondNumber)
+{
+    Console.Write($"max = {firstNumber}, min = {secondNumber}");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.Write($"max = {secondNumber}, min = {firstNumber}");
+}
