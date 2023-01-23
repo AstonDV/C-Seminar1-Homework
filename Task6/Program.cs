653416ee45706d95baa21132fa7d7f93bde62135
AstonDV <astondv.kms@gmail.com>
2023-01-23 20:57:21 +1000

    End task 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.Clear();
+    Console.WriteLine("Да! Ваше число четное!!!");
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Нет, Ваше число нечетное");
+}
